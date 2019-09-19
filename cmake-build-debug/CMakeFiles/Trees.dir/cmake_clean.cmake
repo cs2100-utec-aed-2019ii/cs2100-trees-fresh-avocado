d18597e5ea0a4b60b6edfea52311476f31f7a702
GSR <39766479+fresh-avocado@users.noreply.github.com>
2019-09-19 17:54:05 -0500

    feat(BST class): insert and delete methods in progress

---
+++ cmake-build-debug/CMakeFiles/Trees.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trees.dir/main.cpp.o"
+  "Trees.pdb"
+  "Trees"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
